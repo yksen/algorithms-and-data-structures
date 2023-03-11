7ff8f5d875f3ceb9afa18218cc16aa22e003e9ce
xen <selegakamil@gmail.com>
2023-03-11 21:43:34 +0100

    Add remove to exercise 1

--- list2/src/list2.cpp
+++ list2/src/list2.cpp
@@ -31,6 +31,32 @@ namespace ex1
         *current_root = new Node(key, last_root);
     }
 
+    void remove(Node *&root, int32_t key)
+    {
+        Node **current_root = &root;
+        while (*current_root && (**current_root).key != key)
+            current_root = key > (**current_root).key ? &(**current_root).right : &(**current_root).left;
+
+        if (*current_root == nullptr)
+            return;
+        
+        if ((**current_root).left && (**current_root).right)
+        {
+            Node **min = &(**current_root).right;
+            while ((**min).left)
+                min = &(**min).left;
+
+            (**current_root).key = (**min).key;
+            current_root = min;
+        }
+
+        Node *child = (**current_root).left ? (**current_root).left : (**current_root).right;
+        if (child)
+            child->parent = (**current_root).parent;
+        delete *current_root;
+        *current_root = child;
+    }
+
     TEST(Exercise1, insert)
     {
         Node *root = nullptr;
@@ -51,5 +77,44 @@ namespace ex1
         EXPECT_EQ(root->right->parent->key, 5);
         EXPECT_EQ(root->left->left->parent->key, 3);
         EXPECT_EQ(root->left->right->parent->key, 3);
+    }
+
+    TEST(Exercise1, remove)
+    {
+        Node *root = nullptr;
+        insert(root, 5);
+        insert(root, 3);
+        insert(root, 7);
+
+        remove(root, 3);
+
+        EXPECT_EQ(root->key, 5);
+        EXPECT_EQ(root->left, nullptr);
+        EXPECT_EQ(root->right->key, 7);
+        EXPECT_EQ(root->parent, nullptr);
+        EXPECT_EQ(root->right->parent, root);
+
+        remove(root, 5);
+
+        EXPECT_EQ(root->key, 7);
+        EXPECT_EQ(root->parent, nullptr);
+
+        remove(root, 7);
+
+        EXPECT_EQ(root, nullptr);
+
+        insert(root, 1);
+        insert(root, 3);
+        insert(root, 2);
+        insert(root, 4);
+
+        remove(root, 3);
+
+        EXPECT_EQ(root->key, 1);
+        EXPECT_EQ(root->right->key, 4);
+        EXPECT_EQ(root->right->left->key, 2);
+        EXPECT_EQ(root->right->right, nullptr);
+        EXPECT_EQ(root->parent, nullptr);
+        EXPECT_EQ(root->right->parent, root);
     }
 }

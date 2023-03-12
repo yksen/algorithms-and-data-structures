69738e47adad2d92171773f8a7448b41d004b2d0
xen <selegakamil@gmail.com>
2023-03-12 13:40:08 +0100

    Add exercise 4

--- list2/src/list2.cpp
+++ list2/src/list2.cpp
@@ -198,3 +198,100 @@ namespace ex3
         EXPECT_EQ(root->right->right->key, 6);
     }
 }
+
+namespace ex4
+{
+    struct Node
+    {
+        int32_t key;
+        Node *left;
+        Node *right;
+        Node *parent;
+
+        Node(int32_t key, Node *parent) : key(key), left(nullptr), right(nullptr), parent(parent) {}
+
+        struct iterator
+        {
+            iterator(Node *current) : current(current) {}
+
+            int32_t &operator*() { return current->key; }
+
+            iterator &operator++()
+            {
+                if (current->right)
+                {
+                    current = current->right;
+                    while (current->left)
+                        current = current->left;
+                }
+                else
+                {
+                    Node *parent = current->parent;
+                    while (parent && current == parent->right)
+                    {
+                        current = parent;
+                        parent = parent->parent;
+                    }
+                    current = parent;
+                }
+                return *this;
+            }
+
+            bool operator!=(const iterator &other) { return current != other.current; }
+
+        private:
+            Node *current;
+        };
+
+        iterator begin()
+        {
+            Node *current = this;
+            while (current->left)
+                current = current->left;
+            return iterator(current);
+        }
+
+        iterator end() { return iterator(nullptr); }
+    };
+
+    void insert(Node *&root, int32_t key)
+    {
+        Node **current_root = &root;
+        Node *last_root = nullptr;
+
+        while (*current_root != nullptr)
+        {
+            last_root = *current_root;
+            if (key < (*current_root)->key)
+                current_root = &(*current_root)->left;
+            else
+                current_root = &(*current_root)->right;
+        }
+
+        *current_root = new Node(key, last_root);
+    }
+
+    TEST(Exercise4, BSTiterator)
+    {
+        Node *root = nullptr;
+        insert(root, 3);
+        insert(root, 5);
+        insert(root, 1);
+        insert(root, 2);
+        insert(root, 4);
+
+        int32_t count = 1;
+        for (Node::iterator it = root->begin(); it != root->end(); ++it)
+        {
+            std::cout << *it << " ";
+            EXPECT_EQ(*it, count++);
+        }
+
+        count = 1;
+        for (auto x : *root)
+        {
+            std::cout << x << " ";
+            EXPECT_EQ(x, count++);
+        }
+    }
+}

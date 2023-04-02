64f4cdef566e0ef0d8b126c2068429153c528767
xen <selegakamil@gmail.com>
2023-04-02 15:09:22 +0200

    Solve exercise 9

--- list4/src/list4.cpp
+++ list4/src/list4.cpp
@@ -4,6 +4,85 @@
 #include <numeric>
 #include <random>
 
+namespace ex9
+{
+    struct Node
+    {
+        int32_t value;
+        Node *next;
+
+        Node(int32_t value, Node *next = nullptr) : value(value), next(next) {}
+        Node(std::vector<int32_t> values) : Node(values[0], values.size() > 1 ? new Node(std::vector<int32_t>(values.begin() + 1, values.end())) : nullptr) {}
+    };
+
+    Node *mergeSortedLists(std::vector<Node *> heads)
+    {
+        if (heads.empty())
+            return nullptr;
+
+        Node *merged = nullptr;
+        std::vector<Node *> heap;
+        for (auto head : heads)
+            while (head != nullptr)
+            {
+                heap.push_back(head);
+                std::push_heap(heap.begin(), heap.end(), [](Node *a, Node *b)
+                               { return a->value < b->value; });
+                head = head->next;
+            }
+
+        while (!heap.empty())
+        {
+            std::pop_heap(heap.begin(), heap.end(), [](Node *a, Node *b)
+                          { return a->value < b->value; });
+            Node *node = heap.back();
+            heap.pop_back();
+            node->next = merged;
+            merged = node;
+        }
+
+        return merged;
+    }
+
+    TEST(List4_Exercise9, mergeSortedLists)
+    {
+        std::vector<Node *> heads{
+            new Node(3),
+            new Node(1),
+            new Node(2),
+        };
+        Node *merged = mergeSortedLists(heads);
+        for (int32_t i = 1; i <= 3; i++)
+        {
+            ASSERT_NE(merged, nullptr);
+            EXPECT_EQ(merged->value, i);
+            merged = merged->next;
+        }
+        EXPECT_EQ(merged, nullptr);
+
+        heads = {
+            new Node({1, 3, 5, 7, 9}),
+            new Node({2, 4, 6, 8, 10}),
+            new Node({11, 13, 15, 17, 19}),
+            new Node({12, 14, 16, 18, 20}),
+            new Node({21, 23, 25, 27, 29}),
+            new Node({22, 24, 26, 28, 30}),
+            new Node({31, 33, 35, 37, 39}),
+            new Node({32, 34, 36, 38, 40}),
+            new Node({41, 43, 45, 47, 49}),
+            new Node({42, 44, 46, 48, 50}),
+        };
+        merged = mergeSortedLists(heads);
+        for (int32_t i = 1; i <= 50; i++)
+        {
+            ASSERT_NE(merged, nullptr);
+            EXPECT_EQ(merged->value, i);
+            merged = merged->next;
+        }
+        EXPECT_EQ(merged, nullptr);
+    }
+}
+
 namespace ex10
 {
     uint32_t generateRandomData(const std::string &filePath, uint32_t n)
@@ -41,8 +120,8 @@ namespace ex10
 
     TEST(List4_Exercise10, findMissingNumber)
     {
-        EXPECT_EQ(findMissingNumber("random10.txt"), generateRandomData("random10.txt", 10));
-        EXPECT_EQ(findMissingNumber("random1000.txt"), generateRandomData("random1000.txt", 1000));
-        EXPECT_EQ(findMissingNumber("random1000000.txt"), generateRandomData("random1000000.txt", 1000000));
+        EXPECT_EQ(findMissingNumber("/tmp/random10.txt"), generateRandomData("/tmp/random10.txt", 10));
+        EXPECT_EQ(findMissingNumber("/tmp/random1000.txt"), generateRandomData("/tmp/random1000.txt", 1000));
+        EXPECT_EQ(findMissingNumber("/tmp/random1000000.txt"), generateRandomData("/tmp/random1000000.txt", 1000000));
     }
 }

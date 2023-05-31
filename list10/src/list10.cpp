7bd623f140f773a07cc87eb3829722581d07a400
xen <selegakamil@gmail.com>
2023-05-31 20:29:21 +0200

    Solve exercise 2

--- list10/src/list10.cpp
+++ list10/src/list10.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <stack>
+#include <unordered_set>
 
 namespace ref1
 {
@@ -48,29 +50,17 @@ namespace ref1
     };
 }
 
+typedef std::vector<std::vector<bool>> Grid;
+typedef std::pair<int32_t, int32_t> Point;
+const int32_t dx[] = {-1, 1, 0, 0};
+const int32_t dy[] = {0, 0, -1, 1};
+
 namespace ex1
 {
-    typedef std::vector<std::vector<bool>> Grid;
-    typedef std::pair<int32_t, int32_t> Point;
-
-    void printGrid(bool *t, size_t n)
-    {
-        for (size_t i = 0; i < n; ++i)
-        {
-            for (size_t j = 0; j < n; ++j)
-            {
-                std::cout << t[i * n + j] << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
-
     bool doesPathExist(Grid t, Point start, Point end)
     {
         const size_t n = t.size();
         ref1::UnionFind3 uf(n * n);
-        const int32_t dx[] = {-1, 1, 0, 0};
-        const int32_t dy[] = {0, 0, -1, 1};
 
         for (size_t i = 0; i < n; ++i)
             for (size_t j = 0; j < n; ++j)
@@ -113,5 +103,94 @@ namespace ex1
         EXPECT_TRUE(doesPathExist(t, {0, 0}, {t.size() - 1, t.size() - 2}));
         EXPECT_TRUE(doesPathExist(t, {0, 2}, {t.size() - 1, t.size() - 2}));
         EXPECT_FALSE(doesPathExist(t, {0, 1}, {t.size() - 1, t.size() - 2}));
+    }
+}
+
+namespace ex2
+{
+    size_t getIslandsCount(Grid t)
+    {
+        const size_t n = t.size();
+        ref1::UnionFind3 uf(n * n);
+
+        for (size_t i = 0; i < n; ++i)
+            for (size_t j = 0; j < n; ++j)
+                for (size_t k = 0; k < 4; ++k)
+                {
+                    size_t x = i + dx[k];
+                    size_t y = j + dy[k];
+                    if (x < n && y < n && t[i][j] && t[x][y])
+                        uf.Union(i * n + j, x * n + y);
+                }
+
+        std::unordered_set<int32_t> islands;
+        for (size_t i = 0; i < n; ++i)
+            for (size_t j = 0; j < n; ++j)
+                if (t[i][j])
+                    islands.insert(uf.Find(i * n + j));
+
+        return islands.size();
+    }
+
+    size_t getIslandsCountDFS(Grid t)
+    {
+        const size_t n = t.size();
+        Grid visited(n, std::vector<bool>(n, false));
+
+        size_t islands = 0;
+        for (size_t i = 0; i < n; ++i)
+            for (size_t j = 0; j < n; ++j)
+                if (!visited[i][j] && t[i][j])
+                {
+                    ++islands;
+                    std::stack<Point> toVisit;
+                    toVisit.push({i, j});
+                    while (!toVisit.empty())
+                    {
+                        Point p = toVisit.top();
+                        toVisit.pop();
+                        if (visited[p.first][p.second])
+                            continue;
+                        visited[p.first][p.second] = true;
+                        for (size_t k = 0; k < 4; ++k)
+                        {
+                            size_t x = p.first + dx[k];
+                            size_t y = p.second + dy[k];
+                            if (x < n && y < n && t[x][y])
+                                toVisit.push({x, y});
+                        }
+                    }
+                }
+
+        return islands;
+    }
+
+    TEST(List10_Exercise2, IslandsCount)
+    {
+        Grid t = {
+            {1, 1, 0, 1, 0},
+            {1, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0},
+            {1, 1, 0, 1, 1},
+            {0, 0, 0, 1, 1},
+        };
+        EXPECT_EQ(getIslandsCount(t), 5);
+        EXPECT_EQ(getIslandsCountDFS(t), 5);
+
+        t = {
+            {1, 0, 1},
+            {0, 1, 0},
+            {1, 0, 1},
+        };
+        EXPECT_EQ(getIslandsCount(t), 5);
+        EXPECT_EQ(getIslandsCountDFS(t), 5);
+
+        t = {
+            {1, 1, 1},
+            {1, 1, 0},
+            {1, 0, 1},
+        };
+        EXPECT_EQ(getIslandsCount(t), 2);
+        EXPECT_EQ(getIslandsCountDFS(t), 2);
     }
 }
